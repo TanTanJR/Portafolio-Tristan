4d583b1fe0dccd45430126a60ac6e4ae5be509fc
Tristán Pérez <tristanpv11@gmail.com>
2026-09-13 13:09:23 +0200

    Separa la animación en un archivo JavaScript

---
+++ js/script.js
@@ -0,0 +1,2 @@
+const observer=new IntersectionObserver(entries=>entries.forEach(entry=>{if(entry.isIntersecting)entry.target.classList.add('visible')}),{threshold:.15});
+    document.querySelectorAll('.reveal').forEach(element=>observer.observe(element));
